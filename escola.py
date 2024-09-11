a53ee7c62f4084a17fc94906c87d7f7d8f668b9c
leticiavi2009 <le01042009@gmail.com>
2024-09-11 08:55:38 -0300

    Add files via upload

---
+++ escola.py
@@ -0,0 +1,7 @@
+texto=input("Informe uma diciplina")
+if texto== 'matemática' or 'ingles'==texto:
+    print("gosto")
+elif texto== 'geografia' or 'historia'==texto:
+    print("nao gosto")
+else:
+    print('ok')
